a56f2844f273c3c3155c36cebb97eea2e4f9e0bb
salihmarangoz <salih285@gmail.com>
2022-09-29 01:28:03 +0200

    refactored. can work with bitsets. doesnt work with some images and I don't know why

--- README.md
+++ README.md
@@ -5,11 +5,8 @@ sudo apt install libboost1.71-dev
 
 Ubuntu 20.04
 
-not thread safe !!!
-
 only 2d gridmap !!!
 
-- find closest target
 - smooth path
 - simplify path
 
@@ -18,3 +15,9 @@ add dont check is inside
 note: fibonacci has some problems with handle_type. d_ary_heap has void* handle type.
 
 https://www.boost.org/doc/libs/1_80_0/doc/html/heap/data_structures.html
+
+special implementation for binary grids?
+
+https://theory.stanford.edu/~amitp/GameProgramming/Heuristics.html
+
+Unless having a gridmap of 4GB with uint8_t this algorithm will work just fine.
--- test_astar_gridmap.cpp
+++ test_astar_gridmap.cpp
@@ -1,98 +1,139 @@
 
-#include "astar_gridmap.hpp"
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
+#include "opencv2/imgproc.hpp"
 #include <opencv2/highgui.hpp>
 #include <iostream>
 #include <string>
+#include <chrono>
 
 #include <boost/heap/fibonacci_heap.hpp>
-#include <boost/heap/binomial_heap.hpp> // TODO: segfault. sometimes
-#include <boost/heap/pairing_heap.hpp> // TODO: segfault
+#include <boost/dynamic_bitset.hpp>
+
+// dont define this in your code. use only for debugging accessed nodes on the grid
+#define DEBUG_PATHFINDER_GRID_2D (200)
+#include "pathfinder_grid_2d.hpp"
 
 using namespace cv;
 
-struct NodeA
+Mat load_maze(std::string filename = "../maze_1000_smooth.png")
 {
-    int a;
-
-    NodeA(int a)
-    {
-        this->a = a;
-    }
-
-    bool operator<(NodeA const &rhs) const
-    {
-        return a > rhs.a;
-    }
-};
-
-void test_heap()
-{
-    boost::heap::binomial_heap<NodeA> heap;
-
-    heap.push(NodeA(2));
-    heap.push(NodeA(200));
-    heap.push(NodeA(3));
-    heap.push(NodeA(6));
-    heap.push(NodeA(100));
-    heap.push(NodeA(7));
-    heap.push(NodeA(8));
-    auto handle = heap.push(NodeA(1));
-    heap.push(NodeA(500));
-
-    (*handle).a = 5; // increase value from 1 to 5
-    heap.decrease(handle); // correct!
-    //heap.increase(handle); // incorrect!
-    //heap.update(handle); // correct but slower.
-
-    printf("heap size: %d\n", heap.size());
-
-    int maxiter = heap.size();
-    for (int i = 0; i < maxiter; i++)
-    {
-        auto a = heap.top();
-
-        printf("popped element: %d\n", a.a);
-        heap.pop();
-    }
-}
-
-void test_maze()
-{
-    // std::string filename = "../maze_large.png";
-    std::string filename = "../maze_1000_smooth.png";
-
     Mat img = imread(filename, IMREAD_GRAYSCALE);
     if (img.empty())
     {
         std::cout << "Could not read the image: " << filename << std::endl;
+        exit(1);
+    }
+    return img;
+}
+
+void show_maze(Mat &img, const pathfinder_grid_2d::Path &path, std::string windowName)
+{
+    Mat img_color;
+    cvtColor(img, img_color, COLOR_GRAY2BGR);
+
+    for (int i=0; i<img.rows; i++)
+    {
+        for (int j=0; j<img.cols; j++)
+        {
+            if (img.at<uchar>(i,j) == DEBUG_PATHFINDER_GRID_2D)
+            {
+                Vec3b &color = img_color.at<Vec3b>(i, j);
+                color[0] = 222;
+                color[1] = 222;
+                color[2] = 255;
+            }
+
+        }
+    }
+
+    for (int i = 0; i < path.size(); i++)
+    {
+        Vec3b &color = img_color.at<Vec3b>(path[i].i, path[i].j);
+        color[0] = 0;
+        color[1] = 0;
+        color[2] = 255;
+    }
+
+    namedWindow(windowName, WINDOW_NORMAL);
+    resizeWindow(windowName, 1024, 1024);
+    imshow(windowName, img_color);
+    waitKey(0);
+}
+
+void demo_maze()
+{
+    Mat img = load_maze();
+
+    const auto start = std::chrono::system_clock::now();
+
+    pathfinder_grid_2d::PathFinderGrid2D<uchar> planner(img.data, img.rows, img.cols);
+    pathfinder_grid_2d::Path out;
+    int start_i = 4, start_j = img.cols / 2;
+    int end_i = img.rows - 5, end_j = img.cols / 2;
+    bool success = planner.plan(start_i, start_j, end_i, end_j, out, 127, false);
+
+    const auto end = std::chrono::system_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    printf("demo_maze timing: %ld ms\n", elapsed.count());
+    printf("%d\n", out.size());
+
+    show_maze(img, out, "demo_maze");
+    if (success)
+    {
+        printf("demo_maze plan succesful!\n");
+        
+    }
+    else
+    {
+        printf("demo_maze plan failed!\n");
         return;
     }
 
-    std::vector<std::pair<int, int>> out;
-    astar_gridmap::AstarGridMap2D<uchar> planner(img.data, img.rows, img.cols);
-    bool success = planner.plan(5, img.cols / 2, img.rows - 5, img.cols / 2, out, 127, true);
+}
+
+void demo_maze_bitset()
+{
+    Mat img = load_maze();
+
+    // convert image to bitset
+    boost::dynamic_bitset<> img_bitset;
+    img_bitset.reserve(img.rows*img.cols);
+    for (int i = 0; i < img.rows; i++)
+    {
+        for (int j = 0; j < img.cols; j++)
+        {
+            img_bitset.push_back(img.at<uchar>(i,j));
+        }
+    }
+
+    const auto start = std::chrono::system_clock::now();
+
+    pathfinder_grid_2d::PathFinderGrid2D<bool> planner(&img_bitset, img.rows, img.cols);
+    pathfinder_grid_2d::Path out;
+    int start_i = 5, start_j = img.cols / 2;
+    int end_i = img.rows - 5, end_j = img.cols / 2;
+    bool success = planner.plan(start_i, start_j, end_i, end_j, out, 127, false);
+
+    const auto end = std::chrono::system_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    printf("demo_maze_bitset timing: %ld ms\n", elapsed.count());
 
     if (success)
     {
-        std::cout << "Success!" << out.size() << std::endl;
+        printf("demo_maze_bitset plan succesful!\n");
+        show_maze(img, out, "demo_maze_bitset");
     }
-
-    for (int i = 0; i < out.size(); i++)
+    else
     {
-        img.at<uint8_t>(out[i].first, out[i].second) = 255;
+        printf("demo_maze_bitset plan failed!\n");
+        return;
     }
-
-    namedWindow("Display window", WINDOW_NORMAL);
-    resizeWindow("Display window", 1024, 1024);
-    imshow("Display window", img);
-    waitKey(0);
 }
 
 int main()
 {
-    //test_heap();
-    test_maze();
+    demo_maze();
+    demo_maze_bitset();
     return 0;
 }
